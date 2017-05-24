8402178f46c61663cfc63fe10f5c1eb896ece16a
ConradLetelier <Conrad@Letelier.email>
2017-05-24 14:41:19 +0200

    service package

---
+++ src/main/java/com/grupptre/testverktygbackend/services/LoginService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupptre.testverktygbackend.services;
+
+import com.grupptre.testverktygbackend.models.User;
+import com.grupptre.testverktygbackend.repository.ResTest;
+import java.util.List;
+
+/**
+ *
+ * @author Conrad Letelier <Conrad@Letelier.email>
+ */
+public class LoginService {
+    ResTest rt = new ResTest();
+    /*
+        public User getLoginUser(int id) {
+       // User user = rt.ge
+       return user;
+    }
+*/
+}
