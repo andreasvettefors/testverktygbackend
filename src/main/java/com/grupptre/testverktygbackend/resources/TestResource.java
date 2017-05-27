722ca7012a305ea0b7eb47a18b3f71cffcb8a52b
Andreas Vettefors <contact@vettefors.se>
2017-05-27 20:35:08 +0200

    Småfix
    Har lagt till att man kan få ut en user /Alexiz

--- src/main/java/com/grupptre/testverktygbackend/resources/TestResource.java
+++ src/main/java/com/grupptre/testverktygbackend/resources/TestResource.java
@@ -5,13 +5,7 @@
  */
 package com.grupptre.testverktygbackend.resources;
 
-import com.grupptre.testverktygbackend.models.Test;
-import com.grupptre.testverktygbackend.repository.CourseRepository;
-import com.grupptre.testverktygbackend.repository.UserRepository;
-import java.util.List;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
