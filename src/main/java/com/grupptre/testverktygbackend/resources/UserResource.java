18bb4c58fd0e6dd2e03dcff296b12f39a5a110d8
Alexiz Knöös <qwastanja@hotmail.com>
2017-05-29 13:42:19 +0200

    single user get

--- src/main/java/com/grupptre/testverktygbackend/resources/UserResource.java
+++ src/main/java/com/grupptre/testverktygbackend/resources/UserResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -40,6 +41,13 @@ public class UserResource {
     public User login(User user) {
         LoginService ls = new LoginService();
         return ls.login(user);
+    }
+    
+    @Path("/{id}")
+    @GET
+    public User getUse(@PathParam("id") int id){
+       
+        return us.getUser(id);
     }
     
     @POST
